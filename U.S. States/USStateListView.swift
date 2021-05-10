9f5a01e672908f041f39b90bad2f14fff8e2f031
Giorgio Giannotta <jorge_upa@msn.com>
2021-05-10 15:42:32 +0200

    Done List of the US States

---
+++ U.S. States/USStateListView.swift
@@ -0,0 +1,20 @@
+//
+//  USStateListView.swift
+//  U.S. States
+//
+//  Created by Jorge Giannotta on 10/05/21.
+//
+
+import SwiftUI
+
+struct USStateListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct USStateListView_Previews: PreviewProvider {
+    static var previews: some View {
+        USStateListView()
+    }
+}
---
+++ U.S. States/USStateModel.swift
@@ -0,0 +1,14 @@
+//
+//  USStateModel.swift
+//  U.S. States
+//
+//  Created by Jorge Giannotta on 10/05/21.
+//
+
+import Foundation
+
+struct StateUS: Codable, Identifiable {
+    let id : String
+    let name : String
+    let date : String
+}
