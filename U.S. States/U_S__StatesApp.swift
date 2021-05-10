3b8a5ebc0bd3aa931e3bd0e6d63f06bdbd0e70d8
Giorgio Giannotta <jorge_upa@msn.com>
2021-05-10 13:02:43 +0200

    Initial Commit

---
+++ U.S. States/U_S__StatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  U_S__StatesApp.swift
+//  U.S. States
+//
+//  Created by Jorge Giannotta on 10/05/21.
+//
+
+import SwiftUI
+
+@main
+struct U_S__StatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
