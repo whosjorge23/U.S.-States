ed9cf65ba56fcb919ed5817e25bd7ac1b7081820
Giorgio Giannotta <jorge_upa@msn.com>
2021-05-27 16:12:02 +0200

    Add Circular Tabar

---
+++ U.S. States/Views/CircleTabBarView.swift
@@ -0,0 +1,92 @@
+//
+//  CircleTabBarView.swift
+//  U.S. States
+//
+//  Created by Jorge Giannotta on 27/05/21.
+//
+
+import SwiftUI
+
+struct CircleTabBarView: View {
+    
+//    @State var index = 0
+    @Binding var index : Int
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: nil, content: {
+            Button(action: {
+                self.index = 0
+            }, label: {
+                VStack(alignment: .center, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+                    if self.index != 0 {
+                        Image("tabicon-uslist")
+                    }else {
+                        Image("tabicon-uslist")
+                            .resizable()
+                            .frame(width: 45, height: 43)
+                            .padding()
+                            .background(Color.white.opacity(0.20))
+                            .clipShape(Circle())
+                            .offset(y: -20)
+                            .padding(.bottom, -20)
+                        Text("US States")
+                            .foregroundColor(.orange)
+                    }
+                })
+                
+            })
+            Spacer(minLength: 15)
+            Button(action: {
+                self.index = 1
+            }, label: {
+                VStack(alignment: .center, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+                    if self.index != 1 {
+                        Image("tabicon-usmap")
+                    }else {
+                        Image("tabicon-usmap")
+                            .resizable()
+                            .frame(width: 43, height: 43)
+                            .padding()
+                            .background(Color.white.opacity(0.20))
+                            .clipShape(Circle())
+                            .offset(y: -20)
+                            .padding(.bottom, -20)
+                        Text("USA Map")
+                            .foregroundColor(.orange)
+                    }
+                })
+            })
+            Spacer(minLength: 15)
+            Button(action: {
+                self.index = 2
+            }, label: {
+                VStack(alignment: .center, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+                    if self.index != 2 {
+                        Image("tabicon-person")
+                    }else {
+                        Image("tabicon-person")
+                            .resizable()
+                            .frame(width: 45, height: 43)
+                            .padding()
+                            .background(Color.white.opacity(0.20))
+                            .clipShape(Circle())
+                            .offset(y: -20)
+                            .padding(.bottom, -20)
+                        Text("Info")
+                            .foregroundColor(.orange)
+                    }
+                })
+            })
+            
+        })
+        .padding(.vertical, -10)
+        .padding(.horizontal,25)
+        .animation(.spring())
+    }
+}
+
+//struct CircleTabBarView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CircleTabBarView()
+//    }
+//}
--- U.S. States/Views/ContentView.swift
+++ U.S. States/Views/ContentView.swift
@@ -8,27 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     let stateOfAmerica : [StateUS] = Bundle.main.decode("usstates.json")
+    @State var index = 0
     
     var body: some View {
-            TabView{
-                ListView()
-                    .tabItem {
-                        Image("tabicon-uslist")
-                        Text("US States")
-                    }
-                MapView()
-                    .tabItem {
-                        Image("tabicon-usmap")
-                        Text("USA Map")
-                    }
-                InfoView()
-                    .tabItem {
-                        Image("tabicon-person")
-                        Text("Info")
-                    }
+//            TabView{
+//                ListView()
+//                    .tabItem {
+//                        Image("tabicon-uslist")
+//                        Text("US States")
+//                    }
+//                MapView()
+//                    .tabItem {
+//                        Image("tabicon-usmap")
+//                        Text("USA Map")
+//                    }
+//                InfoView()
+//                    .tabItem {
+//                        Image("tabicon-person")
+//                        Text("Info")
+//                    }
+//            }
+//            .accentColor(.orange)
+        VStack(alignment: .center, spacing: 0, content: {
+            
+            ZStack{
+                if self.index == 0 {
+                    ListView()
+                }
+                else if self.index == 2 {
+                    
+                    InfoView()
+                }
+                else {
+                    MapView()
+                }
             }
-            .accentColor(.orange)
+            .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+            
+            CircleTabBarView(index: self.$index)
+        })
+        .edgesIgnoringSafeArea(.top)
+        
     }
 }
 
